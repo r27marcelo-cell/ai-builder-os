623813208d6c993c37ae0daf7c2f2188b5df1c75
r27marcelo-cell <r27marcelo@gmail.com>
2026-05-02 14:21:11 +0100

    /main.py

--- frontend/backend/main.py
+++ frontend/backend/main.py
@@ -1,1 +1,17 @@
+from fastapi import FastAPI
 
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {
+        "status": "AI Builder OS ativo",
+        "message": "Backend a funcionar corretamente 🚀"
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status": "ok",
+        "system": "online"
+    }
